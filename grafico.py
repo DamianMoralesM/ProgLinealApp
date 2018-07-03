58b35625cab4f11e1bb1ee6d7e33e8fb8f223499
Volderpayoda <volderpayoda@gmail.com>
2018-07-02 22:24:21 -0300

    Cambiando orden de ejecución
    Esto va a servir para cuando tengamos que considerar cantidad variable de restricciones

--- grafico.py
+++ grafico.py
@@ -7,9 +7,9 @@ def f(x, c1, c2, b):
 def graficar(k):
     x = np.arange(0.0, 2.0, 0.01)
     y1 = f(x, k[0][0], k[0][1], k[0][2])
-    y2 = f(x, k[1][0], k[1][1], k[1][2])
-    y3 = f(x, k[2][0], k[2][1], k[2][2])
     pyplot.plot(x, y1)
+    y2 = f(x, k[1][0], k[1][1], k[1][2])
     pyplot.plot(x, y2)
+    y3 = f(x, k[2][0], k[2][1], k[2][2])
     pyplot.plot(x, y3)
     pyplot.show()
